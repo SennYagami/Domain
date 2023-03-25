068a24c70dba932994f7641819965929720ae028
rick <506322829@qq.com>
2023-03-25 08:46:01 +0800

    add resolvers module

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,59 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-ethers";
+import "@openzeppelin/hardhat-upgrades";
+import "@nomiclabs/hardhat-etherscan";
+require("dotenv").config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    compilers: [
+      { version: "0.6.12" },
+      { version: "0.5.17" },
+      { version: "0.7.6" },
+      { version: "0.8.17" },
+    ],
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 100,
+      },
+    },
+  },
+  networks: {
+    hardhat: { allowUnlimitedContractSize: true },
+    mumbai: {
+      chainId: 80001,
+      url: process.env.ALCHEMY_API_MUMBAI,
+      accounts: [
+        process.env.DEPLOYER as string,
+        process.env.MULTISIGWALLETOWNER1 as string,
+        process.env.MULTISIGWALLETOWNER2 as string,
+        process.env.MULTISIGWALLETOWNER3 as string,
+        process.env.ACCEPTER as string,
+      ],
+      gas: 10000000,
+      //   allowUnlimitedContractSize: true,
+    },
+    polygon: {
+      chainId: 137,
+      url: process.env.ALCHEMY_API,
+      accounts: [
+        process.env.DEPLOYER as string,
+        process.env.MULTISIGWALLETOWNER1 as string,
+        process.env.MULTISIGWALLETOWNER2 as string,
+        process.env.MULTISIGWALLETOWNER3 as string,
+        process.env.ACCEPTER as string,
+      ],
+      gas: 10000000,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: process.env.ETHERSCAN_API_KEY as string,
+      polygon: process.env.ETHERSCAN_API_KEY as string
+    }
+  }
 };
 
 export default config;
