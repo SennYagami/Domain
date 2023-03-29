4213d2cf4c0e944f7e4bf6747329d24a3fea210e
rick <506322829@qq.com>
2023-03-29 19:13:55 +0800

    add ReverseRegistrar

--- contracts/resolvers/DefaultReverseResolver.sol
+++ contracts/resolvers/DefaultReverseResolver.sol
@@ -1,47 +1,26 @@
 pragma solidity ^0.8.17;
 
-import "../registry/DONS.sol";
-// import "@ensdomains/ens/contracts/ReverseRegistrar.sol";
+import "../registry/DID.sol";
 
-/**
- * @dev Provides a default implementation of a resolver for reverse records,
- * which permits only the owner to update it.
- */
 contract DefaultReverseResolver {
     // namehash('addr.reverse')
     bytes32 constant ADDR_REVERSE_NODE = 0x91d1777781884d03a6757a803996e38de2a42967fb37eeaca72729271025a9e2;
 
-    DONS public dons;
+    DID public did;
     mapping (bytes32 => string) public name;
 
-    /**
-     * @dev Only permits calls by the reverse registrar.
-     * @param node The node permission is required for.
-     */
+  
     modifier onlyOwner(bytes32 node) {
-        require(msg.sender == dons.owner(node));
+        require(msg.sender == did.owner(node));
         _;
     }
 
-    /**
-     * @dev Constructor
-     * @param donsAddr The address of the DONS registry.
-     */
-    constructor(DONS donsAddr) {
-        dons = donsAddr;
-
-        // Assign ownership of the reverse record to our deployer
-        // ReverseRegistrar registrar = ReverseRegistrar(ens.owner(ADDR_REVERSE_NODE));
-        // if (address(registrar) != address(0x0)) {
-        //     registrar.claim(msg.sender);
-        // }
+    
+    constructor(DID didAddr) {
+        did = didAddr;
     }
 
-    /**
-     * @dev Sets the name for a node.
-     * @param node The node to update.
-     * @param _name The name to set.
-     */
+   
     function setName(bytes32 node, string memory _name) public onlyOwner(node) {
         name[node] = _name;
     }
--- contracts/resolvers/PublicResolver.sol
+++ contracts/resolvers/PublicResolver.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.8.17;
 pragma experimental ABIEncoderV2;
 
-import "../registry/DONS.sol";
+import "../registry/DID.sol";
 import "./profiles/ABIResolver.sol";
 import "./profiles/AddrResolver.sol";
 import "./profiles/ContentHashResolver.sol";
@@ -12,46 +12,25 @@ import "./profiles/PubkeyResolver.sol";
 import "./profiles/TextResolver.sol";
 import "./profiles/CommissonResolver.sol";
 
-/**
- * A simple resolver anyone can use; only allows the owner of a node to set its
- * address.
- */
-contract PublicResolver is ABIResolver, AddrResolver, ContentHashResolver, DNSResolver, InterfaceResolver, NameResolver, PubkeyResolver, TextResolver, CommissonResolver {
-    DONS dons;
 
-    /**
-     * A mapping of authorisations. An address that is authorised for a name
-     * may make any changes to the name that the owner could, but may not update
-     * the set of authorisations.
-     * (node, owner, caller) => isAuthorised
-     */
+contract PublicResolver is ABIResolver, AddrResolver, ContentHashResolver, DNSResolver, InterfaceResolver, NameResolver, PubkeyResolver, TextResolver, CommissonResolver {
+    DID did;
+
     mapping(bytes32=>mapping(address=>mapping(address=>bool))) public authorisations;
 
     event AuthorisationChanged(bytes32 indexed node, address indexed owner, address indexed target, bool isAuthorised);
 
-    constructor(DONS _dons) {
-        dons = _dons;
+    constructor(DID _did) {
+        did = _did;
     }
 
-    /**
-     * @dev Sets or clears an authorisation.
-     * Authorisations are specific to the caller. Any account can set an authorisation
-     * for any name, but the authorisation that is checked will be that of the
-     * current owner of a name. Thus, transferring a name effectively clears any
-     * existing authorisations, and new authorisations can be set in advance of
-     * an ownership transfer if desired.
-     *
-     * @param node The name to change the authorisation on.
-     * @param target The address that is to be authorised or deauthorised.
-     * @param isAuthorised True if the address should be authorised, or false if it should be deauthorised.
-     */
     function setAuthorisation(bytes32 node, address target, bool isAuthorised) external {
         authorisations[node][msg.sender][target] = isAuthorised;
         emit AuthorisationChanged(node, msg.sender, target, isAuthorised);
     }
 
     function isAuthorised(bytes32 node) internal override view returns(bool) {
-        address owner = dons.owner(node);
+        address owner = did.owner(node);
         return owner == msg.sender || authorisations[node][owner][msg.sender];
     }
 
